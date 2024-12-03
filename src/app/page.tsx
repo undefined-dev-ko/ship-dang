67f2a21216506882b57ddc7052bb8ab703ad1a35
전병길 <jeonbg@kakao.com>
2024-12-03 22:27:05 +0900

    🔥 remove(component): 테스트 Drawer 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 import Link from 'next/link';
 
-import { MainDrawer } from './ui/main-drawer';
-
 export default function Home() {
   return (
     <div className="mx-auto flex h-full max-w-screen-sm flex-col bg-slate-50 px-2">
       <span className="font-notoSansKR text-xl text-zinc-800">Home</span>
-      <MainDrawer />
 
       <ul>
         {Array.from({ length: 10 }).map((_, index) => (
